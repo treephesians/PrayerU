e7fded534857db3cc2a3fe4dff9c7e38bb0af9ee
ephesi4ns <ephesi4ns623@gmail.com>
2023-12-14 23:45:17 +0900

    DONE 2:43:20

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -49,7 +49,42 @@ function PostThread({ userId }:{ userId: string}){
             accountId: userId,
         }
     })
-    return <h1>Post Thread From</h1>
+
+    const onSubmit = async () => {
+        // await createThread
+    }
+
+    return (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} 
+          className="mt-10 flex flex-col justify-start gap-10">
+
+          <FormField
+              control={form.control}
+              name="thread"
+              render={({ field }) => (
+                <FormItem className="flex flex-col w-full gap-3 ">
+                  <FormLabel className="text-base-semibold text-light-2">
+                    Content
+                  </FormLabel>
+                  <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                    <Textarea
+                        rows={15}
+                        className="account-form_input no-focus"
+                        {...field}
+                     />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="bg-primary-500">
+                Post Thread
+            </Button>
+        </form>
+        </Form>
+    )
 }
 
 export default PostThread;
